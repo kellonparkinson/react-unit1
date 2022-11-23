69263863b2ba6fae307aa473bd8fac4019caa680
Kellon Parkinson <kellon.digital@gmail.com>
2022-11-22 17:24:02 -0700

    completed tictactoe project

--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -1,11 +1,25 @@
 import React from 'react'
 
 const Square = (props) => {
-  // console.log(props.squares, props.player)
+  const { squares, setSquares, player, setPlayer, squareValue, index } = props
+
+  const handleClick = () => {
+    if (!squareValue) {
+      if (player) {
+        squares.splice(index, 1, 'X')
+        setSquares(squares)
+        setPlayer(!player)
+      } else {
+        squares.splice(index, 1, 'O')
+        setSquares(squares)
+        setPlayer(!player)
+      }
+    }
+  }
 
   return (
-    <div>
-      
+    <div className='square' onClick={handleClick} >
+      {squareValue === 'O' ? 'O' : squareValue}
     </div>
   )
 }
